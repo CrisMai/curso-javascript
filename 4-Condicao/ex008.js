3c8d363ae4553b84657361197ed4ff397a19ba90
Cris Mai <crismai.dev@gmail.com>
2021-11-22 18:10:13 -0300

    Condição Simples

---
+++ 4-Condicao/ex008.js
@@ -0,0 +1,12 @@
+
+var vel = '76'
+console.log(`A velocidade de seu carro é ${vel} km/h`)
+if (vel > 60) { 
+    console.log(`Você ultrapassou o limite de velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga com cuidado!`)
+//Exemplo de uma condição SIMPLES.
+/*Só tem um tipo de bloco - o true. É representada pelo if.
+if(condição){
+    true
+}*/
