37f9eca112da18ebe44ab3381f60b98eb88592c9
Cris Mai <crismai.dev@gmail.com>
2021-11-22 18:15:22 -0300

    Condição Composta

---
+++ 4-Condicao/ex009.js
@@ -0,0 +1,12 @@
+
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é brasileiro!')   
+} else {
+    console.log('Você é estrangeiro!')
+}
+//Exemplo de uma condição COMPOSTA.
+/*Nessa condição temos o bloco true e o bloco false.
+Caso seja true vai executar uma ação, caso seja false vai fazer outra
+ação. É representada pelo if e else.*/
