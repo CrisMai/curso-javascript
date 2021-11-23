5d473981cd8b64ecf5023ef525ecd44b41da2c92
Cris Mai <crismai.dev@gmail.com>
2021-11-23 08:16:25 -0300

    Sort()

---
+++ 11-VariaveisCompostas/ex01.js
@@ -0,0 +1,15 @@
+
+/*let num = [5, 3, 2, 1, 6]
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)*/
+
+let num = [5, 6, 3, 2, 4, 1]
+let pos = num.indexOf(3)
+    console.log(`O valor 3 está na posição ${pos}`)
+num. sort()
+num.push(8)
+
+
+for(let pos = 0; pos < num.length; pos++)
+    console.log(`A posição ${pos} tem o valor ${num [pos]}`) //Código tradicional do percurso para exibição do vetor.
