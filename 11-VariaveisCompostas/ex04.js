104bac85256deb69ac90762afc4fbabcff291ef8
Cris Mai <crismai.dev@gmail.com>
2021-11-23 08:18:10 -0300

    Ordenação Reversa

---
+++ 11-VariaveisCompostas/ex04.js
@@ -0,0 +1,19 @@
+
+//Ordenação Reversa dos elementos do array:
+
+var cidades = ["Chapecó", "Palhoça", "Lages", "Florianópolis"]
+
+cidades.reverse()
+for(i = 0; i < cidades.length; i++) {
+    console.log(cidades[i] + " ") 
+}
+
+//Ordenação Alfabética Reversa:
+
+/*var cidades = ["Chapecó", "Palhoça", "Lages", "Florianópolis"]
+
+cidades.sort()
+cidades.reverse()
+for(i = 0; i < cidades.length; i++) {
+    console.log(cidades[i] + " ") 
+}*/
