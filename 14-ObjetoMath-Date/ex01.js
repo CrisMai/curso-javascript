cb2583ba4dc61f49cc796114a8a360f88ef0abeb
Cris Mai <crismai.dev@gmail.com>
2021-11-26 16:15:13 -0300

    Exercício 01

---
+++ 14-ObjetoMath-Date/ex01.js
@@ -0,0 +1,18 @@
+
+console.log(Math.PI);
+console.log("Logaritmo natural de 10: " + Math.LN10);
+console.log("Raiz quadrada de 2: " + Math.SQRT1_2);
+
+var valor = 10.3;
+var mt = Math.ceil(valor);
+console.log(mt);
+
+var valor = 10.8;
+var mt = Math.floor(valor);
+console.log(mt);
+
+var valor = 4;
+var mt = Math.pow(valor, 2);
+console.log(mt);
+
+
